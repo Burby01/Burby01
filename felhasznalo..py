84cffe58056cc4eda2129ba33de652c2f349b769
Burby01 <11c-ternyak@ipari.vein.hu>
2024-11-15 13:43:12 +0100

    alakulgat a felhasznalo

--- felhasznalo..py
+++ felhasznalo..py
@@ -6,15 +6,19 @@ root = tk.Tk()
 root.title("Könyv Lista")
 root.geometry("500x500")
 
-konyv_cimek = [konyv["cim"] for konyv in konyvek_adatok]  
+elotte = tk.Label(root, text="Válassza ki a könyvet:")
+elotte.pack(padx=10, pady=10)
+
+
+konyv_cimek = [konyv["cim"] for konyv in konyvek_adatok]
 combo_box = ttk.Combobox(root, values=konyv_cimek)
 combo_box.pack(padx=10, pady=10)
 
 
-def show_book_info(event):
-    kivalasztott_cim = combo_box.get()  
+def konyv_info():
+    kivalasztott_cim = combo_box.get()
     kivalasztott_konyv = next((konyv for konyv in konyvek_adatok if konyv["cim"] == kivalasztott_cim), None)
-    
+
     if kivalasztott_konyv:
         info_text = f"Cím: {kivalasztott_konyv['cim']}\n"
         info_text += f"Szerző: {kivalasztott_konyv['szerzo']}\n"
@@ -24,12 +28,14 @@ def show_book_info(event):
         info_text += f"ISBN: {kivalasztott_konyv['isbn']}"
     else:
         info_text = "Nincs könyv kiválasztva."
-    
+
     info_label.config(text=info_text)
 
+adat_button = tk.Button(root, text="A könyv adatai", command=konyv_info)
+adat_button.pack(padx=10, pady=10)
 
-combo_box.bind("<<ComboboxSelected>>", show_book_info)
-
+kolcsonzes_button = tk.Button(root,text="Kikölcsönzés",command=konyv_info)
+kolcsonzes_button.pack(padx=10,pady=10)
 
 info_label = tk.Label(root, text="Válassz ki egy könyvet.", justify=tk.LEFT)
 info_label.pack(padx=10, pady=10)
