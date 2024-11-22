de8f19633b07eded058cb93f1549645029c975c5
BotKovacs7 <kovbot.07@gmail.com>
2024-11-22 11:57:45 +0100

    mukodes

---
+++ admin.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+from tkinter import ttk
+from konyveklista import konyvek_adatok
+
+root = tk.Tk()
+root.title("Könyv Lista")
+root.geometry("500x500")
+
+elotte = tk.Label(root, text="Válassza ki a könyvet:")
+elotte.pack(padx=10, pady=10)
+
+konyv_cimek = [konyv["cim"] for konyv in konyvek_adatok]
+combo_box = ttk.Combobox(root, values=konyv_cimek, state="readonly")
+combo_box.pack(padx=10, pady=10)
+
+def konyv_info():
+    kivalasztott_cim = combo_box.get()
+    kivalasztott_konyv = next((konyv for konyv in konyvek_adatok if konyv["cim"] == kivalasztott_cim), None)
+
+    if kivalasztott_konyv:
+        valaszt_text = f"Cím: {kivalasztott_konyv['cim']}\n"
+        valaszt_text += f"Szerző: {kivalasztott_konyv['szerzo']}\n"
+        valaszt_text += f"Kiadás éve: {kivalasztott_konyv['megjelenes']}\n"
+        valaszt_text += f"Kiadó: {kivalasztott_konyv['kiado']}\n"
+        valaszt_text += f"Oldalszám: {kivalasztott_konyv['oldalszam']}\n"
+        valaszt_text += f"ISBN: {kivalasztott_konyv['isbn']}"
+    else:
+        valaszt_text = "Nincs könyv kiválasztva."
+
+    valaszt_label.config(text=valaszt_text)
+
+adat_button = tk.Button(root, text="A könyv adatai", command=konyv_info)
+adat_button.pack(padx=10, pady=10)
+    
+valaszt_label = tk.Label(root, text="Még nem választott ki könyvet , kérem válasszon ki.", justify=tk.LEFT)
+valaszt_label.pack(padx=10, pady=10)
+
+root.mainloop()
